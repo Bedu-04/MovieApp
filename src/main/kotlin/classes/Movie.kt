64ded279f63fcf41cb2d0be081de9985b452e0e8
JILSE7 <saidnnnn@gmail.com>
2023-03-16 22:31:17 -0600

    Clases
    Se empieza a crear la clase pelicula

---
+++ src/main/kotlin/classes/Movie.kt
@@ -0,0 +1,19 @@
+package main.kotlin.classes
+
+import java.util.Date
+
+class Movie(
+    name: String,
+    actors: List<String>,
+    duration: Double,
+    gender: String, // ESTO EN UN ENUM
+    directors: List<String>,
+    date: String,
+    rating: Double,
+    language: String,
+
+) {
+    // Agregar a tu lista
+
+    // Eliminar de tu lista
+}
