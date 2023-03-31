59d4358ffef67e353c45376cf38bc93d6582c6eb
JILSE7 <saidnnnn@gmail.com>
2023-03-30 21:40:52 -0600

    add comments in functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,22 +4,8 @@ import main.kotlin.classes.App
 import main.kotlin.classes.Movie
 
 fun main(args: Array<String>) {
-
-    // Clase calendario
     val welcomeMenu = WelcomeMenu()
     welcomeMenu.showMenu()
-    val app = App()
-
-    runBlocking {
-        app.loadMovies()
-    }
-
-    app.showMenu()
-
-
-
-
-
 }
 
 
